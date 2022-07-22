0e365905674c8a33737b5b38946b8da026fff5b1
muhammet Çelik <89980402+Mclk1@users.noreply.github.com>
2022-07-22 10:31:39 +0300

    Update postgres_operator_v01.py

--- postgres_operator_v01.py
+++ postgres_operator_v01.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 default_args={
-    'owner':'coder2j',
+    'owner':'clk',
     'retries':5,
     'retry_delay':timedelta(minutes=5)
 }
